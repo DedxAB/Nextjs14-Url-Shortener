201584ac7f96496edc13a6077150281bdc69e45c
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-05-22 01:15:26 +0530

    Add error, loading, and not-found components

--- app/url/[id]/page.jsx
+++ app/url/[id]/page.jsx
@@ -5,7 +5,6 @@ export default async function page({ params }) {
   const { id } = params;
   const data = await fetchUrlById(id);
   const actualData = data?.data ? data.data : null;
-//   console.log(actualData);
 
   if (actualData) {
     redirect(actualData?.originalUrl);
