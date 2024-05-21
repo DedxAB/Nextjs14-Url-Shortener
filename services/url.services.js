d37a6db24394861d8ec3a2cd858106ac93fb30f4
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-05-22 02:24:05 +0530

    Add fetchUrlDataById function to fetch URL data by ID

--- services/url.services.js
+++ services/url.services.js
@@ -14,3 +14,18 @@ export const fetchUrlById = async (id) => {
     console.log(`Error fetching URL by ID: ${error}`);
   }
 };
+
+export const fetchUrlDataById = async (id) => {
+  try {
+    const res = await fetch(`${baseUrl}/api/short-url/${id}/analysis`, {
+      cache: "no-store",
+    });
+    if (!res.ok) {
+      const { error } = await res.json();
+      throw new Error(error);
+    }
+    return await res.json();
+  } catch (error) {
+    console.log(`Error fetching URL data by ID: ${error}`);
+  }
+};
