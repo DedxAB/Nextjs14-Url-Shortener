f9fc4082bc06a03fe4b2f9556457eaa1efab0448
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-05-22 11:40:58 +0530

    Add URL validation function

---
+++ validations/url.validations.js
@@ -0,0 +1,10 @@
+export const validateUrl = (url) => {
+  const urlRegex =
+    /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/;
+
+  if (url.match(urlRegex)) {
+    return true;
+  } else {
+    return false;
+  }
+};
